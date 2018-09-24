877bff46faaf40c7bfe0dd113477522eb2c7b518
husni habil <habilhusni@gmail.com>
2018-09-25 03:49:36 +0700

    Test CMS IDX.

--- src/App.js
+++ src/App.js
@@ -1,47 +1,14 @@
-import React, { Component } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch,
-} from 'react-router-dom';
+import React, { Component } from "react";
 // import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
-import Data from './Components/Data';
-import Home from './Components/Home';
-import Login from './Components/Login';
-import AddFilm from './Components/AddFilm';
-import EditFilm from './Components/EditFilm';
-import NoMatch from './Components/NoMatch';
-
+import Home from "./Components/Home";
 
 // const data = [1, 2, 3]
 class App extends Component {
-
   render() {
-    return (
-      <Router>
-        <div>
-          <nav className="nav">
-            <div className="nav-right nav-menu">
-              <a className="nav-item"><Link to="/">Home</Link></a>
-              <a className="nav-item"><Link to="/login">Login</Link></a>
-            </div>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/data" component={Data} />
-            <Route exact path="/add" component={AddFilm} />
-            <Route exact path="/edit" component={EditFilm} />
-            <Route component={NoMatch} />
-          </Switch>
-        </div>
-      </Router>
-    );
+    return <Home />;
   }
-
 }
 
 export default App;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,44 +1,180 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { Component } from "react";
+import axios from "axios";
+import "../Home.css";
 
-const styles = {
-  container: {
-    textAlign: "center"
-  },
-  buttonChoice: {
-    display: "inline",
-    position: "relative",
-    left: "41%"
-  },
-  button: {
-    marginLeft: "30px",
-    marginTop: "30px"
-  },
-  button2: {
-    marginTop: "30px"
+class Home extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      Id: 0,
+      Category: "",
+      Title: "",
+      Date: "",
+      URLPhoto: "",
+      Description: "",
+      resId: 0,
+      resCategory: "",
+      resTitle: "",
+      resDate: "",
+      resURLPhoto: "",
+      resDescription: ""
+    };
   }
-};
 
-const Home = props => (
-  <section className="hero is-medium is-primary is-bold">
-    <div className="hero-body">
-      <div className="container" style={styles.container}>
-        <span className="icon">
-          <i className="fa fa-home" />
-        </span>
-        <h1 className="title">SWAPI</h1>
-        <h2 className="subtitle">The Star Wars API husni habil</h2>
+  handleGetNews = () =>
+    axios
+      .get("https://desolate-forest-78881.herokuapp.com/news")
+      .then(function(response) {
+        console.log(response.data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+  handlePostNews = () => {
+    const { Id, Category, Title, Date, URLPhoto, Description } = this.state;
+    const that = this;
+
+    return axios
+      .post("https://desolate-forest-78881.herokuapp.com/news", {
+        Id: Id,
+        Category: Category,
+        Title: Title,
+        Date: Date,
+        URLPhoto: URLPhoto,
+        Description: Description
+      })
+      .then(function(response) {
+        console.log(response);
+        that.setState({
+          resId: response.data.Id,
+          resCategory: response.data.Category,
+          resTitle: response.data.Title,
+          resDate: response.data.Date,
+          resURLPhoto: response.data.URLPhoto,
+          resDescription: response.data.Description
+        });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  };
+
+  handleDeleteNews = () =>
+    axios
+      .delete("https://desolate-forest-78881.herokuapp.com/news/2")
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+
+  handleInputId = e => {
+    this.setState({
+      Id: e.target.value
+    });
+  };
+
+  handleInCategory = e => {
+    this.setState({
+      Category: e.target.value
+    });
+  };
+
+  handleInTitle = e => {
+    this.setState({
+      Title: e.target.value
+    });
+  };
+
+  handleInDate = e => {
+    this.setState({
+      Date: e.target.value
+    });
+  };
+
+  handleInURLPhoto = e => {
+    this.setState({
+      URLPhoto: e.target.value
+    });
+  };
+
+  handleInDescription = e => {
+    this.setState({
+      Description: e.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>News List</h2>
+        <table>
+          <tr>
+            <th>Id</th>
+            <th>Category</th>
+            <th>Title</th>
+            <th>Date</th>
+            <th>URLPhoto</th>
+            <th>Description</th>
+          </tr>
+          <tr>
+            <td> {this.state.resId} </td>
+            <td> {this.state.resCategory} </td>
+            <td> {this.state.resTitle} </td>
+            <td> {this.state.resDate} </td>
+            <td> {this.state.resURLPhoto} </td>
+            <td> {this.state.resDescription} </td>
+          </tr>
+        </table>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-evenly",
+            width: 200,
+            marginTop: 50,
+            marginLeft: 50,
+            height: 300
+          }}
+        >
+          <input
+            placeholder="Id"
+            value={this.state.Id}
+            onChange={this.handleInputId}
+          />
+          <input
+            placeholder="Category"
+            value={this.state.Category}
+            onChange={this.handleInCategory}
+          />
+          <input
+            placeholder="Title"
+            value={this.state.Title}
+            onChange={this.handleInTitle}
+          />
+          <input
+            placeholder="Date"
+            value={this.state.Date}
+            onChange={this.handleInDate}
+          />
+          <input
+            placeholder="URLPhoto"
+            value={this.state.URLPhoto}
+            onChange={this.handleInURLPhoto}
+          />
+          <input
+            placeholder="Description"
+            value={this.state.Description}
+            onChange={this.handleInDescription}
+          />
+          <button onClick={this.handlePostNews}>Add News!</button>
+        </div>
       </div>
-      <div className="buttonChoice" style={styles.buttonChoice}>
-        <a className="button is-info" style={styles.button2}>
-          <Link to="/data">Show Films</Link>
-        </a>
-        <a className="button is-warning" style={styles.button}>
-          <Link to="/add">Add Film</Link>
-        </a>
-      </div>
-    </div>
-  </section>
-);
+    );
+  }
+}
 
 export default Home;
